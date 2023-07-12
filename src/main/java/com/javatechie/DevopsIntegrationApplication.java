9762790290c653816238cb512c88977764dafd47
anhvv <anhvv13@gmail.com>
2023-07-12 10:15:40 +0700

    sua 2

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "WELCOME TO DEVOPS SHACK";
+		return "WELCOME TO DEVOPS SHACK 2";
 	}
 
 	public static void main(String[] args) {
