09a94f17c5a34a461f35531650d7d8e0eedcaeb8
anhvv <anhvv13@gmail.com>
2023-07-13 16:04:06 +0700

    update 2

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "WELCOME TO DEVOPS SHACK 11";
+		return "WELCOME TO DEVOPS SHACK 12";
 	}
 
 	public static void main(String[] args) {
